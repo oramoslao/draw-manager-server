8b9fa984c8096c3ce21880cd8ea8b539cda75f58
Usuario <Usuario@DESKTOP-DLSF6MN>
2020-02-17 10:32:54 -0500

    update popùlate-prizes.sql

--- src/DrawManager.ImportTool/Data/Sql/populate-prizes.sql
+++ src/DrawManager.ImportTool/Data/Sql/populate-prizes.sql
@@ -1,22 +1,61 @@
 DELETE FROM Prizes;
 
-INSERT INTO "Prizes" VALUES (1, 'Primer premio US$ 10000.00', 'Primer premio US$ 10000.00', 0, NULL, 5);
-INSERT INTO "Prizes" VALUES (2, 'Segundo premio US$ 5000.00', 'Segundo premio US$ 5000.00', 0, NULL, 5);
-INSERT INTO "Prizes" VALUES (3, 'Tercer premio US$ 1000.00', 'Tercer premio US$ 1000.00', 0, NULL, 5);
-INSERT INTO "Prizes" VALUES (4, 'Cuarto premio US$ 1000.00', 'Cuarto premio US$ 1000.00', 0, NULL, 5);
-INSERT INTO "Prizes" VALUES (5, 'Quinto premio US$ 1000.00', 'Quinto premio US$ 1000.00', 0, NULL, 5);
-INSERT INTO "Prizes" VALUES (6, 'Sexto premio US$ 1000.00', 'Sexto premio US$ 1000.00', 0, NULL, 5);
-INSERT INTO "Prizes" VALUES (7, 'Séptimo premio US$ 1000.00', 'Séptimo premio US$ 1000.00', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (1, 'Ganador - $10mil', 'Ganador - $10mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (2, 'Suplente 1 - $10mil', 'Suplente 1 - $10mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (3, 'Suplente 2 - $10mil', 'Suplente 2 - $10mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (4, 'Ganador - $5mil', 'Ganador - $5mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (5, 'Suplente 1 - $5mil', 'Suplente 1 - $5mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (6, 'Suplente 2 - $5mil', 'Suplente 2 - $5mil', 0, NULL, 5);
 
-INSERT INTO "Prizes" VALUES (8, 'Primer premio US$ 5000.00', 'Primer premio US$ 5000.00', 0, NULL, 1);
-INSERT INTO "Prizes" VALUES (9, 'Segundo premio US$ 2500.00', 'Segundo premio US$ 2500.00', 0, NULL, 1);
+INSERT INTO "Prizes" VALUES (7, 'Ganador 1 - $1mil', 'Ganador 1 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (8, 'Suplente 1.1 - $1mil', 'Suplente 1.1 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (9, 'Suplente 1.2 - $1mil', 'Suplente 1.2 - $1mil', 0, NULL, 5);
 
-INSERT INTO "Prizes" VALUES (10, 'Primer premio US$ 5000.00', 'Primer premio US$ 5000.00', 0, NULL, 2);
-INSERT INTO "Prizes" VALUES (11, 'Segundo premio US$ 2500.00', 'Segundo premio US$ 2500.00', 0, NULL, 2);
+INSERT INTO "Prizes" VALUES (10, 'Ganador 2 - $1mil', 'Ganador 2 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (11, 'Suplente 2.1 - $1mil', 'Suplente 2.1 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (12, 'Suplente 2.2 - $1mil', 'Suplente 2.2 - $1mil', 0, NULL, 5);
 
-INSERT INTO "Prizes" VALUES (12, 'Primer premio US$ 5000.00', 'Primer premio US$ 5000.00', 0, NULL, 3);
-INSERT INTO "Prizes" VALUES (13, 'Segundo premio US$ 2500.00', 'Segundo premio US$ 2500.00', 0, NULL, 3);
+INSERT INTO "Prizes" VALUES (13, 'Ganador 3 - $1mil', 'Ganador 3 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (14, 'Suplente 3.1 - $1mil', 'Suplente 3.1 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (15, 'Suplente 3.2 - $1mil', 'Suplente 3.2 - $1mil', 0, NULL, 5);
 
-INSERT INTO "Prizes" VALUES (14, 'Primer premio US$ 5000.00', 'Primer premio US$ 5000.00', 0, NULL, 4);
-INSERT INTO "Prizes" VALUES (15, 'Segundo premio US$ 2500.00', 'Segundo premio US$ 2500.00', 0, NULL, 4);
+INSERT INTO "Prizes" VALUES (16, 'Ganador 4 - $1mil', 'Ganador 4 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (17, 'Suplente 4.1 - $1mil', 'Suplente 4.1 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (18, 'Suplente 4.2 - $1mil', 'Suplente 4.2 - $1mil', 0, NULL, 5);
 
+INSERT INTO "Prizes" VALUES (19, 'Ganador 5 - $1mil', 'Ganador 5 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (20, 'Suplente 5.1 - $1mil', 'Suplente 5.1 - $1mil', 0, NULL, 5);
+INSERT INTO "Prizes" VALUES (21, 'Suplente 5.2 - $1mil', 'Suplente 5.2 - $1mil', 0, NULL, 5);
+
+
+INSERT INTO "Prizes" VALUES (22, 'Ganador - $5mil', 'Ganador - $5mil', 0, NULL, 1);
+INSERT INTO "Prizes" VALUES (23, 'Suplente 1 - $5mil', 'Suplente 1 - $5mil', 0, NULL, 1);
+INSERT INTO "Prizes" VALUES (24, 'Suplente 2 - $5mil', 'Suplente 2 - $5mil', 0, NULL, 1);
+
+INSERT INTO "Prizes" VALUES (25, 'Ganador - $2.5mil', 'Ganador - $2.5mil', 0, NULL, 1);
+INSERT INTO "Prizes" VALUES (26, 'Suplente 1 - $2.5mil', 'Suplente 1 - $2.5mil', 0, NULL, 1);
+INSERT INTO "Prizes" VALUES (27, 'Suplente 2 - $2.5mil', 'Suplente 2 - $2.5mil', 0, NULL, 1);
+
+INSERT INTO "Prizes" VALUES (28, 'Ganador - $5mil', 'Ganador - $5mil', 0, NULL, 2);
+INSERT INTO "Prizes" VALUES (29, 'Suplente 1 - $5mil', 'Suplente 1 - $5mil', 0, NULL, 2);
+INSERT INTO "Prizes" VALUES (30, 'Suplente 2 - $5mil', 'Suplente 2 - $5mil', 0, NULL, 2);
+
+INSERT INTO "Prizes" VALUES (31, 'Ganador - $2.5mil', 'Ganador - $2.5mil', 0, NULL, 2);
+INSERT INTO "Prizes" VALUES (32, 'Suplente 1 - $2.5mil', 'Suplente 1 - $2.5mil', 0, NULL, 2);
+INSERT INTO "Prizes" VALUES (33, 'Suplente 2 - $2.5mil', 'Suplente 2 - $2.5mil', 0, NULL, 2);
+
+INSERT INTO "Prizes" VALUES (34, 'Ganador - $5mil', 'Ganador - $5mil', 0, NULL, 3);
+INSERT INTO "Prizes" VALUES (35, 'Suplente 1 - $5mil', 'Suplente 1 - $5mil', 0, NULL, 3);
+INSERT INTO "Prizes" VALUES (36, 'Suplente 2 - $5mil', 'Suplente 2 - $5mil', 0, NULL, 3);
+
+INSERT INTO "Prizes" VALUES (37, 'Ganador - $2.5mil', 'Ganador - $2.5mil', 0, NULL, 3);
+INSERT INTO "Prizes" VALUES (38, 'Suplente 1 - $2.5mil', 'Suplente 1 - $2.5mil', 0, NULL, 3);
+INSERT INTO "Prizes" VALUES (39, 'Suplente 2 - $2.5mil', 'Suplente 2 - $2.5mil', 0, NULL, 3);
+
+INSERT INTO "Prizes" VALUES (40, 'Ganador - $5mil', 'Ganador - $5mil', 0, NULL, 4);
+INSERT INTO "Prizes" VALUES (41, 'Suplente 1 - $5mil', 'Suplente 1 - $5mil', 0, NULL, 4);
+INSERT INTO "Prizes" VALUES (42, 'Suplente 2 - $5mil', 'Suplente 2 - $5mil', 0, NULL, 4);
+
+INSERT INTO "Prizes" VALUES (43, 'Ganador - $2.5mil', 'Ganador - $2.5mil', 0, NULL, 4);
+INSERT INTO "Prizes" VALUES (44, 'Suplente 1 - $2.5mil', 'Suplente 1 - $2.5mil', 0, NULL, 4);
+INSERT INTO "Prizes" VALUES (45, 'Suplente 2 - $2.5mil', 'Suplente 2 - $2.5mil', 0, NULL, 4);
