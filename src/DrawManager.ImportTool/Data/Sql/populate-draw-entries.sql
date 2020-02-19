929ab20f593d2f56b3b0b07cffc4d8e81c40a3bd
Usuario <Usuario@DESKTOP-DLSF6MN>
2020-02-19 00:29:16 -0500

    fix populate draw entries SQL script

--- src/DrawManager.ImportTool/Data/Sql/populate-draw-entries.sql
+++ src/DrawManager.ImportTool/Data/Sql/populate-draw-entries.sql
@@ -5,5 +5,5 @@ UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='DrawEntries';
 INSERT INTO DrawEntries ([DrawId], [EntrantId], [RegisteredOn])
 SELECT dr.Id AS [ DrawId ], e.Id AS [ EntrantId ], DATETIME('now')
 FROM "Data" AS d
-	INNER JOIN Entrants AS e ON e.Code = d.Identificacion
-	INNER JOIN Draws AS dr ON dr.Name = d.Provincia;
+	LEFT JOIN Entrants AS e ON e.Code = d.Identificacion AND e.City = d.Provincia
+	LEFT JOIN Draws AS dr ON dr.Name = d.Provincia;
