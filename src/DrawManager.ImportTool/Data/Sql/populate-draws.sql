59301904408c6dd8556c444f73bfc5b09e3b269a
Usuario <Usuario@DESKTOP-DLSF6MN>
2020-02-19 09:10:26 -0500

    Minor changes

--- src/DrawManager.ImportTool/Data/Sql/populate-draws.sql
+++ src/DrawManager.ImportTool/Data/Sql/populate-draws.sql
@@ -2,7 +2,8 @@ DELETE FROM "Draws";
 
 UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='Draws';
 
-INSERT INTO "Draws" ([Name], [Description], [AllowMultipleParticipations], [ProgrammedFor])
-SELECT [Provincia] as [Name], [Provincia] as [Description], 0 as [AllowMultipleParticipations], '2020-02-20' as [ProgrammedFor] FROM "Data" GROUP BY [Provincia];
+INSERT INTO "Draws" ([Name], [Description], [AllowMultipleParticipations], [ProgrammedFor], [GroupName])
+SELECT [Provincia] as [Name], [Provincia] as [Description], 0 as [AllowMultipleParticipations], '2020-02-20' as [ProgrammedFor], 'SORTEO_2020' as [GroupName] 
+FROM "Data" GROUP BY [Provincia];
 
 SELECT * FROM "Draws"
