2b1a812cc3591d22d9efa1ab22702ad681089fad
Luis Javier Vallejo Mursuli <javico02@gmail.com>
2020-02-19 13:02:44 -0500

    correccion de los archivos sql a la hora de importar la data

--- src/DrawManager.ImportTool/Data/Sql/populate-entrants.sql
+++ src/DrawManager.ImportTool/Data/Sql/populate-entrants.sql
@@ -4,7 +4,7 @@ UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='Entrants';
 
 
 INSERT INTO "Entrants" ([Code], [Name], [City])
-SELECT [Identificacion] ,[Nombre] ,[Provincia] FROM "Data" GROUP BY [Identificacion], [Nombre], [Provincia];
+SELECT [Identificacion] ,[Nombre] , 'NACIONAL' FROM "Data" GROUP BY [Identificacion], [Nombre];
 
 
 SELECT * FROM "Entrants";
